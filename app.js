087dcd51c63496696f73534b7c43a41dfb20fe7f
Dhanarajan Sugumar <dhanarajan.sugumar@gmail.com>
2020-04-27 23:35:56 -0500

    adding a new item to UI

--- app.js
+++ app.js
@@ -71,7 +71,9 @@ var UIController = (function() {
         typeString: '.add__type',
         descString: '.add__description',
         valueString: '.add__value',
-        addBtnString: '.add__btn'
+        addBtnString: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list'
     };
 
     return {
@@ -81,6 +83,24 @@ var UIController = (function() {
                 description: document.querySelector(DOMStrings.descString).value,
                 value: document.querySelector(DOMStrings.valueString).value
             }
+        },
+        addListItems: function(obj, type) {
+            var html, newHtml;
+            // Create HTML String with placeholder text
+            if (type === "inc") {
+                element = DOMStrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === "exp") {
+                element = DOMStrings.expenseContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            // Replace the placeholder text with actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // insert the HTML into DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
         getDOMStrings: function(a) {
             return DOMStrings;
@@ -111,9 +131,9 @@ var Controller = (function(bdgtCtrl, UICtrl) {
 
         // 2. Add the item to the budget contoller
         var newItem = bdgtCtrl.addItem(inputData.type, inputData.description, inputData.value);
-        console.log(newItem);
 
         // 3. Add the item to the UI
+        UICtrl.addListItems(newItem, inputData.type);
 
         // 4. Calculate the Budget
 
